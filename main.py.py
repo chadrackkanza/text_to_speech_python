948e2b6645e6bd3fc958424d4e5df6a907559c44
Chadrack KANZA <chadrackkanza2015@gmail.com>
2021-08-07 09:13:23 +0100

    first commit

---
+++ main.py.py
@@ -0,0 +1,29 @@
+#Kinshasa, 07/08/2021
+#Chadrack KANZA
+
+import pyttsx3
+engine = pyttsx3.init()
+
+"""VOLUME"""
+volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
+engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
+
+""" RATE"""
+rate = engine.getProperty('rate')   # getting details of current speaking rate
+engine.setProperty('rate', 125)     # setting up new voice rate
+
+"""VOICE"""
+voices = engine.getProperty('voices')       #getting details of current voice
+#engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
+
+names = ['chadrack Kanza','John','Peter', 'Sarah', 'Thomas','Anderson', 'Dorcas']
+def reader_name(namesList):
+    for name in namesList:
+        engine.say(name)
+
+engine.say("These persons can come")
+reader_name(names)
+engine.runAndWait()
+engine.stop()
+
